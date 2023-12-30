fc314550796dd9a5d5815f422fedc5bcd618c351
Faith Okoth <faithokoth@Faiths-MacBook-Pro.local>
2023-12-30 15:50:59 +0300

    implemented load habits view

--- client/App.tsx
+++ client/App.tsx
@@ -1,20 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import LoadHabits from './src/component/loadHabits';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    <LoadHabits/>
+
+
+      {/*<Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />*/}
+
+  </View>  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#black',
     alignItems: 'center',
     justifyContent: 'center',
+  
   },
 });
---
+++ client/src/component/loadHabits.tsx
@@ -0,0 +1,100 @@
+import React, {useState} from "react";
+import { StyleSheet, View, Text, FlatList, TouchableOpacity, } from "react-native";
+
+interface ListItem {
+    id: string;
+    label: string;
+}
+const LoadHabits: React.FC = () => {
+    //loads habits from server
+    //temporary data
+    const [checkedItems, setCheckedItems] = useState<string[]>([]);
+    const habits: ListItem[] = [
+        {id: "1", label: "read a book"},
+        {id: "2", label:  "play"},
+        {id: "3", label: "30 minutes sermon"}];
+    
+    const handleCheckedBoxToggle = (itemId: string) => {
+        const updatedCheckedItems = checkedItems.includes(itemId)
+        ? checkedItems.filter((item)=> item != itemId)
+        : [... checkedItems, itemId];
+        setCheckedItems(updatedCheckedItems)
+    };
+
+    const renderItem = ({item}: {item: ListItem}) => {
+        return(<TouchableOpacity style={styles.itemHold} onPress={()=> handleCheckedBoxToggle(item.id)}>
+            <View style={[styles.checkedCheckBox, checkedItems.includes(item.id) ? styles.checkedCheckBox: styles.uncheckedCheckbox]}>
+                {checkedItems.includes(item.id) && (
+                    <View/>
+                )}  
+            </View>
+            <Text style={styles.text}>{item.label}</Text>
+        </TouchableOpacity>)
+    }
+    return (
+        <View style={styles.container}>
+            <FlatList 
+            data={habits}
+            keyExtractor={(item) => item.id}
+            
+            renderItem={renderItem}
+
+           
+/>
+        
+            </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        display: "flex",
+        width: 388,
+        padding:10,
+        flexDirection: "column",
+        alignItems: "center"
+
+        
+    },
+    itemHold: {
+        width: 387,
+        height: 72,
+        flexShrink: 0,
+        borderRadius: 20,
+        borderWidth: 2,
+        padding: 10,
+        borderColor: "#D6D6D6",
+        alignItems: "baseline"
+
+
+    }, 
+    checkbox: {
+        width: 20,
+        height: 20,
+        borderRadius: 5,
+        borderWidth: 2,
+        borderColor: "#D6D6D6",
+        marginRight: 10,
+        justifyContent: "center",
+        alignItems: "center",
+    
+    },
+    checkedCheckBox:{
+        backgroundColor:"#OOF"
+    },
+    uncheckedCheckbox:{
+        backgroundColor: "transparent"
+    },
+    text: {
+        color: 'black',
+        fontFamily: "Rubik",
+        fontSize: 16,
+        fontStyle: "normal",
+        fontWeight: "400",
+        //textDecorationLine: "line-through"
+
+    }
+})
+export default LoadHabits;
+
