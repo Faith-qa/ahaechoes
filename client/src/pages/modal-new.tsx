aab1c1c8d150ffad9c22ffc6bb73590265007e57
Faith Okoth <faithokoth@Faiths-MacBook-Pro.local>
2024-01-26 15:50:07 +0300

    fixed modal bug

--- client/src/pages/modal-new.tsx
+++ client/src/pages/modal-new.tsx
@@ -1,33 +1,70 @@
-import React from "react";
-import { Modal, View, StyleSheet, Pressable } from "react-native";
+import React, { useState } from "react";
+import { Modal, View, StyleSheet, Pressable, Text, } from "react-native";
 import { Feather } from '@expo/vector-icons';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-const New: React.FC = () => {
+interface NewProps {
+    visible: boolean
+}
+
+const New: React.FC<NewProps> = ({visible}) => {
+   const [isvisible, setIsvisible] = useState(visible);
+
+   if(!visible)  {
+    return null;
+   }
+  
+   
     return(
-        <View >
-            <Modal style={styles.container}>
-                <Pressable style={styles.XContainer}>
-                <Feather name="x-circle" size={24} color="black" style={styles.xbutton} />
+            <Modal 
+            animationType="slide"
+            visible={!isvisible}
+            transparent={true}>
+                <View style={styles.mcontainer}>
+                <Pressable style={styles.XContainer} onPress={()=>{setIsvisible(!visible)}} >
+                <Feather name="x-circle" size={24} color="black"/>
                 </Pressable>
                 <View style={styles.content}>
                 <MaterialCommunityIcons name="clipboard-text-clock-outline" size={24} color="black" style={styles.clipicon} />
-                <View></View>
+                <View style={styles.contentText}>
+                    <View style={styles.titleCont}>
+                        <Text style={styles.title}>What would you like to do today?</Text>
+                    </View>
+                    <View style={styles.messageCont}>
+                        <Text style={styles.message}>In the journey of adulting, we often face tasks
+                        we'd rather not do but must. Embrace the discipline over  motivation,
+                        whether in  organizing yout space or mastering meal prep.
+                        Adulting is doing what's necessary, not just what's desired</Text>
+                    </View>
+
                 </View>
 
+                </View>
+                <Pressable style={styles.taskCont}><Text style={styles.taskText}>New Task</Text></Pressable>
+                <Pressable style={styles.taskCont}><Text style={styles.taskText}>New Task</Text></Pressable>
+
+                </View>
+
+                
 
             </Modal>
-        </View>
 
         
     )
 };
 
 const styles = StyleSheet.create({
-container: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'flex-start',
+    container:{
+        alignItems: "flex-end",
+        display: "flex"
+
+    },
+mcontainer: {
+    flex: 0,
+    //flexDirection: 'column',
+    alignItems: 'center',
+    alignSelf: "center",
+    justifyContent: "center",
     padding: 35,
     paddingTop: 20,
     paddingRight: 20,
@@ -35,20 +72,20 @@ container: {
     paddingLeft: 20,
     gap: 35,
     borderRadius: 12,
-    backgroundColor: "#FFF"
+    backgroundColor: "yellow"
 },
 XContainer: {
     display: "flex",
     justifyContent: "flex-end",
-    alignItems: "center",
+    //alignItems: "center",
     gap: 10,
     alignSelf: "stretch"
 },
 xbutton: {
-    display: "flex",
-    width: 20,
+    //display: "flex",
+    //width: 20,
     height: 14,
-    flexDirection: "column",
+    //flexDirection: "column",
     justifyContent: "center",
     flexShrink: 0
 },
@@ -78,6 +115,54 @@ contentText: {
     flexDirection: "column",
     alignItems: "center",
     gap: 5
+},
+titleCont: {
+    width: 260,
+    height: 22
+},
+title:{
+    color: '#02113C', // Default color or use your variable if it's dynamic
+    textAlign: 'center',
+    fontFamily: 'SF Pro Text',
+    fontSize: 17,
+    fontStyle: 'normal',
+    fontWeight: '600',
+    lineHeight: 22,
+    letterSpacing: -0.073,
+},
+messageCont:{
+    width: 260,
+    height: 60
+},
+message: {
+    color: '#4E5876', // Default color or use your variable if it's dynamic
+    textAlign: 'center',
+    fontFamily: 'SF Pro Text',
+    fontSize: 15,
+    fontStyle: 'normal',
+    fontWeight: '400',
+    lineHeight: 20,
+    letterSpacing: -0.23,
+},
+taskCont:{
+    display: 'flex',
+    width: 260,
+    height: 44,
+    paddingVertical: 12,
+    paddingHorizontal: 91,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 8,
+    backgroundColor: "blue"
+},
+taskText:{
+    color: 'black',
+    fontFamily: 'SF Pro Text',
+    fontSize: 15,
+    fontStyle: 'normal',
+    fontWeight: '600',
+    lineHeight: 20,
+    letterSpacing: -0.23,
 }
 })
 
--- client/src/screens/home.tsx
+++ client/src/screens/home.tsx
@@ -1,20 +1,29 @@
-import React from "react";
-import { View, StyleSheet, Pressable } from "react-native";
+import React, { useState } from "react";
+import { View, StyleSheet, Pressable, Modal } from "react-native";
 import LoadHabits from "../component/loadHabits";
 import Search from "../component/search";
 import DaysOfWeekButtons from "../component/thisWeek";
 import { AntDesign } from '@expo/vector-icons';
+import New from "../pages/modal-new";
+import { useNavigation } from "@react-navigation/native";
 
 
 const Home: React.FC = () =>{
+    const [visible, setVisible] = useState(false);
+    const visiblity = () => {
+        setVisible(true)
+    }
+
 
     return(
         <View style={styles.container}>
             <DaysOfWeekButtons/>
             <Search/>
             <LoadHabits/>
+            <New visible={visible}/>
             <View style={styles.plusbutton}>
-            <Pressable>
+            
+            <Pressable onPress={visiblity} >
             <AntDesign name="pluscircle" size={45} color="#DFBD43" />
 
             </Pressable>
