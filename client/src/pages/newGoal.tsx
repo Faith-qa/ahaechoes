289bc89142f1bb8ce12480842e14c62f701e6dca
Faith Okoth <faithokoth@Faiths-MacBook-Pro.local>
2024-02-10 10:57:39 +0300

    fixed picker component in form page

--- client/src/pages/newGoal.tsx
+++ client/src/pages/newGoal.tsx
@@ -1,7 +1,9 @@
+import { Picker } from "@react-native-picker/picker";
 import React, { useEffect, useState } from "react";
-import { View, Modal, StyleSheet, Pressable, Text, TextInput } from "react-native";
-import { Ionicons, MaterialIcons, FontAwesome} from '@expo/vector-icons';
-import {Picker} from '@react-native-picker/picker';
+import { Controller, useForm } from "react-hook-form";
+import { Modal, View, Text, TextInput, StyleSheet, Pressable, } from "react-native";
+import { getCurrentTime } from "../../utils/date";
+
 
 interface NewProps {
     newGoal: boolean,
@@ -9,88 +11,66 @@ interface NewProps {
     onClose: () => void
 
 }
-const NewGoal: React.FC<NewProps> = ({newGoal, closeGoal, onClose})=>{
-    const [tvisible, setvisible] = useState(false);
-    const [habitCount, setHabitCount] = useState(1); // Initial count, assuming you start with one daily habit
+const NewGoal: React.FC<NewProps> = ({newGoal, closeGoal, onClose}) => {
+    const [gvisible, setvisible] = useState(false);
+    const [habitKind, setHabitKind] = useState<any>('daily');
 
-    
+
+    //load the modal
     useEffect(()=>{
-        setvisible(newGoal)
-        
+        setvisible(newGoal);
     }, [newGoal]);
+    
+    //set form and form requirements
 
-    const newdailyhabit = (index: number) => {
-       return ( <View style={styles.newhabit}>
-            <TextInput style={styles.input2} />
-            <Pressable onPress={handleAddHabit}><MaterialIcons name="add-box" size={50} color="#DFBD43" /></Pressable>
-            <Pressable onPress={()=>{handleRemoveHabit(index)}}><FontAwesome name="remove" size={24} color="#DFBD43" /></Pressable>
-        </View>)
+    const {control, handleSubmit, formState: {errors}} = useForm({
+        defaultValues:{
+            goal: "",
+            habitKind: "",
+            habits: [],
+            tracker: {
+                test: ""
+            },
+            motivations: []     
+    }})
 
-
-    }
-    const handleRemoveHabit = (index: number) => {
-        setHabitCount((prevCount) => prevCount - 1);
-       /* setHabits((prevHabits) => {
-          const updatedHabits = [...prevHabits];
-          updatedHabits.splice(index, 1);
-          return updatedHabits;
-        });*/
-      };
-      
-
-    const handleAddHabit = () => {
-        setHabitCount((prevCount) => prevCount + 1);
-      };
-    const closeModal = () =>{
+    const onSubmit = (data: any) => {
+        console.log(data);
         closeGoal();
         onClose();
-    }
+    } 
+    return (<Modal animationType="slide"
+    visible={gvisible}
+    transparent={true}>
+        <View style={styles.container}>
+            <Text style={styles.heading}>New Goal</Text>
+        <Text style={styles.title}>
+            What goal do you have in mind today
+        </Text>
+        <Controller
+        control={control}
+        render={({field: { onChange, onBlur, value }}) => (
+            <TextInput
+            style={styles.titleCont}
+            onBlur={onBlur}
+            onChangeText={(text)=> onChange(text)}
+            value={value}/>)}
+        name="goal"
+        rules={{required: true}}/>
+        <Text style={styles.title}>What kind of habits do you want to cultivate to help you achive this goal(daily, weekly, monthly)</Text>
+        <Picker
+        style={{width: 150}}
+        selectedValue={habitKind}
+        onValueChange={(itemValue, itemIndex)=> setHabitKind(itemValue)}>
+            <Picker.Item label="daily" value="daily"/>
+            <Picker.Item label="weekly" value="weekly"/>
+            <Picker.Item label="monthly " value="monthly"/>
 
-    return(
-        <Modal
-        visible={tvisible}>
-            <View style={styles.container}>
-            <Pressable style={styles.XContainer} onPress={closeModal}>
-            <Ionicons name="arrow-back-circle-outline" size={24} color="black" />
+        </Picker>
+      
+    </View></Modal>)
 
-            </Pressable>
-                <Text style={styles.heading}>
-                    Create New Goal
-                </Text>
-
-                <Text style={styles.title}>What goal do you have in mind</Text>
-                <TextInput style={styles.input}/>
-                <Text style={styles.title}>What daily habits can you cultivate to achieve this goal</Text>
-               {/*} <View style={styles.newhabit}>
-                    <TextInput style={styles.input2} />
-                    <Pressable onPress={newdailyhabit}><MaterialIcons name="add-box" size={50} color="#DFBD43" /></Pressable>
-    </View>*/}
-                {[...Array(habitCount)].map((_, index) => (
-          <React.Fragment key={index}>{newdailyhabit(index)}</React.Fragment>
-        ))}
-                <Text style={styles.title}>What timelines are you giving yourself.</Text>
-                <Picker>
-                    <Picker.Item label= "3 months" value="3 months"/>
-                    <Picker.Item label= "6 months" value="6 months"/>
-                    <Picker.Item label="1 year" value="I year"/>
-                </Picker>
-                <Text style={styles.title}>In momments of doubt what would
-                be your source of motivation</Text>
-                <TextInput style={styles.DetailsCont}/>
-                <View style={styles.button}>
-                <Pressable
-                    >
-                        <Text style={styles.buttonText}>Create</Text>
-                    </Pressable>
-                </View>
-            
-                
-            </View>
-
-        </Modal>
-    )
-};
-
+}
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -128,52 +108,8 @@ const styles = StyleSheet.create({
         marginBottom:20,
         //alignSelf:"flex-start"
     },
-    DetailsCont: {
-        width : 358,
-        height: 82,
-        flexShrink: 0,
-        borderWidth: 1,
-        //borderRadius: 20,
 
-        borderColor: 'rgba(0, 0, 0, 0.40)',
-        backgroundColor: '#FFFFFF',
-        shadowColor: 'rgba(255, 255, 255, 0.14)',
-        shadowOffset: {
-          width: 0,
-          height: 16,
-        },
-        shadowRadius: 24,
-        shadowOpacity: 1,
-        elevation: 1, // For Android shadow
-        padding: 10, // Adjust padding based on your design
-        color: "black",
-        fontFamily: "Inter",
-       // fontSize: 11,
-        fontStyle: "normal",
-        fontWeight: "400",
-        lineHeight: 18.857
-
-},
-    button: {
-        width: 358,
-        height: 67,
-        flexShrink: 0,
-        backgroundColor: "#DFBD43",
-        alignItems: "center",
-        justifyContent: "center",
-        margin: 20
-
-    },
-    buttonText:{
-        color: "#444",
-        fontFamily:"Inter",
-        fontSize: 18,
-        fontStyle:"normal",
-        fontWeight: "600",
-        lineHeight: 38,
-
-    },
-    input: {
+    titleCont: {
         width: 358,
         height: 48,
         flexShrink: 0,
@@ -186,49 +122,11 @@ const styles = StyleSheet.create({
           width: 0,
           height: 16,
         },
-        shadowRadius: 24,
+        //shadowRadius: 24,
         shadowOpacity: 1,
         elevation: 1, // For Android shadow
-        padding: 10, 
-        color: "black",
-        fontFamily: "Inter",
-        fontStyle: "normal",
-        fontWeight: "400",
-        lineHeight: 18.857,
+        padding: 10, // Adjust padding based on your design
+      }
+    })
 
-    },
-    newhabit:{
-        flexDirection: "row",
-        alignSelf: "flex-start",
-        gap: 10
-
-    },
-    input2: {
-        width: 250,
-        height: 50,
-        flexShrink: 0,
-        //borderRadius: 20,
-        borderWidth: 1,
-        borderColor: 'rgba(0, 0, 0, 0.40)',
-        backgroundColor: '#FFFFFF',
-        shadowColor: 'rgba(255, 255, 255, 0.14)',
-        shadowOffset: {
-          width: 0,
-          height: 16,
-        },
-        shadowRadius: 24,
-        shadowOpacity: 1,
-        elevation: 1, // For Android shadow
-        padding:10 , 
-        color: "black",
-        fontFamily: "Inter",
-        fontStyle: "normal",
-        fontWeight: "400",
-        lineHeight: 18.857,
-        //alignSelf: "flex-start"
-
-    },
-
-})
-
-export default NewGoal;
+    export default NewGoal;
