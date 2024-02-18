5491fa72f7d078e70da2bb5a88d3b245056614d7
Faith Okoth <faithokoth@Faiths-MacBook-Pro.local>
2024-02-18 16:10:08 +0300

    implemented new ui to access journals

--- client/src/pages/More/journaling/myjournals.tsx
+++ client/src/pages/More/journaling/myjournals.tsx
@@ -1,21 +1,53 @@
 import React, { useEffect, useState } from "react";
-import { View } from "react-native";
+import { useAnimatedValue, View, Text, Modal, Pressable } from "react-native";
+import { Entypo, MaterialIcons,FontAwesome } from '@expo/vector-icons';
+import Takevideo from "./videojournaling";
 
 interface NewProps {
     visible: boolean,
     onClose: () => void
 }
 
-const myJournals: React.FC<NewProps> =({visible})=>{
+const MyJournals: React.FC<NewProps> =({visible, onClose})=>{
     const [opened, setopened] = useState(false)
+    const [newVideo, isNewVideo] = useState(false);
+    const [newAudio, isNewAudio] = useState(false);
+    const [newDoc, isNewDoc] = useState(false);
+
 
     useEffect(()=>{
         setopened(visible);
     }, [visible]);
-
     
+    // handle video modal
 
-    return(<View></View>);
+    const openVideo = () =>{
+        isNewVideo(true);
+    }
+
+    const closeVideo = () => {
+        isNewVideo(false);
+    }
+
+
+    return(<View>
+        {opened ? <View>
+        <Text>pick an input mode.</Text>
+        <View>
+            <Pressable onPress={openVideo}>
+            <Entypo name="video-camera" size={60} color="black" />
+            </Pressable>
+            <Pressable>
+            <MaterialIcons name="audiotrack" size={24} color="black" />
+            </Pressable>
+            <Pressable>
+            <FontAwesome name="pencil-square-o" size={24} color="black" />
+            </Pressable>
+            <Takevideo newVideo={newVideo} closeVideo={closeVideo}/>
+
+        </View>
+    </View>: "" }</View>);
 
 }
 
+export default MyJournals;
--- client/src/screens/more.tsx
+++ client/src/screens/more.tsx
@@ -1,18 +1,43 @@
-import React from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Image, Pressable } from "react-native";
+import MyJournals from "../pages/More/journaling/myjournals";
 import Takevideo from "../pages/More/journaling/videojournaling";
 
 const More: React.FC = () => {
+    const [visible, setVisible] = useState(false);
+
+    const journal = () =>{
+        setVisible(true);
+    }
+
+    const closejournal = () =>{
+        setVisible(false);
+    }
+
     return (
         <View style={styles.container}>
             <View style={styles.imageCont}>
-            <Image source={{uri: 'https://images.pexels.com/photos/18340828/pexels-photo-18340828/free-photo-of-man-in-traditional-north-american-indigenous-clothing.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load'}}
+            {/*<Image source={{uri: 'https://images.pexels.com/photos/18340828/pexels-photo-18340828/free-photo-of-man-in-traditional-north-american-indigenous-clothing.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load'}}
         style={styles.image} />
 
             <Text>
                 Hi,
-            </Text>
-            <Takevideo/>
+    </Text>*/}
+            <View style={styles.jContainer}>
+                <Text style={[styles.jText, {fontSize: 30, fontWeight: "normal"}]}>Your journals</Text>
+                <Pressable style={styles.mcont} onPress={()=>journal()}>
+                    <Text style={[styles.jText, {alignSelf: "center"}]} >New Entry</Text>
+                </Pressable>
+            </View>
+            <MyJournals visible={visible} onClose={closejournal}/>
+            <View style={styles.jContainer}>
+            <Text style={[styles.jText, {fontSize: 30, fontWeight:"normal"}]}>Get inspired...</Text>
+            </View>
+            
+                <Image source={{uri:'https://unsplash.com/photos/black-microphone-on-white-background-gKKB13zJ1ss'}}/>
+            <View>
+
+            </View>
             </View>
         </View>
     );
@@ -41,6 +66,30 @@ const styles = StyleSheet.create({
         padding: 20,
 
         
+    },
+    jContainer :{
+        width: 300,
+        alignItems: "center",
+        justifyContent: "space-between",
+        flexDirection:"row",
+        margin: 10,
+        padding: 10,
+        gap: 35
+
+    },
+    jText:{
+        fontSize: 16,
+        fontWeight: "bold",
+        color: "#4D4117",
+
+    },
+    mcont:{
+        width: 100,
+        height: 50,
+        borderRadius: 20,
+        backgroundColor:"#DFBD43",
+        alignItems: "center",
+        padding: 12
     }
 })
 
