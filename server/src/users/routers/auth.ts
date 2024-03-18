9ba479c154653e18fd83510d0ce097007a781caa
Faith Okoth <faithokoth@Faiths-MacBook-Pro.local>
2024-03-18 17:04:26 +0300

    implementend jwt token requirement on login

--- server/src/users/routers/auth.ts
+++ server/src/users/routers/auth.ts
@@ -3,13 +3,14 @@ import * as express from 'express';
 import {registerValidation, loginValidation} from '../validation/userValidation'
 import * as bcryptjs from 'bcryptjs';
 import * as jwt from 'jsonwebtoken';
+import * as dotenv from 'dotenv';
 
 
-
+dotenv.config();
 //set up a router
 const router = express.Router();
 
-router.post('signup', async(req, res)=>{
+router.post('/signup', async(req, res)=>{
     //check and validate input
     const {error} = registerValidation(req.body);
 
@@ -46,5 +47,36 @@ router.post('signup', async(req, res)=>{
         res.status(201).send(savedUser);
     }catch(err){
         res.status(400).send({message: err})}
+
+})
+
+//log in
+
+router.post('/login', async(req, res)=>{
+    //check and validate input
+    const {error} = registerValidation(req.body);
+
+    if (error){
+        return res.status(400).send({message: error['details'][0]['message']})
+    }
+
+    //validation 2 check if user exist in database
+
+    const user = await User.findOne({email: req.body.email});
+
+    if (!user){
+        return res.status(400).send({message:"user not found"})
+    }
+
+    //validation 3: check password
+
+    const passValidation = await bcryptjs.compare(req.body.password, user.password);
+    if(!passValidation){
+        return res.status(400).send({message: "invalid password"})
+    }
+    const token:string = jwt.sign({_id: user._id},process.env.TOKEN_SECRET);
+    
+    return res.header('auth-token', token).send({"auth-token": token});
+
 
 })
