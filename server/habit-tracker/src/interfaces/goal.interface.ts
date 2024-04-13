b935bf30652972917bce903e890ca1fc853f8909
Faith-qa <atienofaith12@gmail.com>
2024-04-13 21:22:06 +0300

    created interfaces for the database

---
+++ server/habit-tracker/src/interfaces/goal.interface.ts
@@ -0,0 +1,22 @@
+import { Document, Schema } from "mongoose";
+
+export interface Goal extends Document {
+   user: Schema.Types.ObjectId;
+   goal: string;
+   habitKind: 'daily' | 'weekly' | 'monthly';
+   tracker: {
+       daily?:{
+           time: Date;
+           reminderDays: string[];
+       };
+       weekly?: {
+           reminderDay: string;
+           time: Date;
+       };
+       monthly?:{
+           week: number;
+           day: string;
+           time: Date;
+       }
+   }
+}
