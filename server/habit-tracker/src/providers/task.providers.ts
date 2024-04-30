533de6ddc9a2cc5a313cdb31ca2df8d6d305abb6
Faith-qa <atienofaith12@gmail.com>
2024-04-30 21:50:50 +0300

    fixed routing errors

--- server/habit-tracker/src/providers/task.providers.ts
+++ server/habit-tracker/src/providers/task.providers.ts
@@ -1,7 +1,7 @@
 import {Connection} from "mongoose";
 import {TaskSchema} from "../schemas/task.schema";
 
-export const goalsProviders = [
+export const TaskProviders = [
     {
         provide: 'TASK_MODEL',
         useFactory: (connection: Connection)=> connection.model('TASK', TaskSchema),
