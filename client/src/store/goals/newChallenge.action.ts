739687f98971be3f5800ed715a857b96b865f148
Faith-qa <atienofaith12@gmail.com>
2024-06-11 18:09:27 +0300

    git add set up redux slice for state management

--- client/src/store/goals/newChallenge.action.ts
+++ client/src/store/goals/newChallenge.action.ts
@@ -1,6 +1,8 @@
-import {createAsyncThunk} from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import {retrieveToken} from "../auth/Authorization";
+import axios from "axios";
 
-
+const api_URL = process.env.EXPO_PUBLIC_API_URI;
 interface BaseChallenge{
     user: string;
     challenge: string;
@@ -24,9 +26,43 @@ interface monthlyChallenge extends BaseChallenge{
 }
 
 type newChallengeRegistration = DailyChallenge | weeklyChallenge | monthlyChallenge;
+interface ThunkArg {
+    challengeData: newChallengeRegistration;
+    userId: string;
+}
+
 
 
 //create challenge
+export const creatChallenge = createAsyncThunk(
+    'goal/new',
+    async ({challengeData, userId}: ThunkArg, {rejectWithValue})=> {
+
+        try{
+            //get token from async storage
+            const token = retrieveToken();
+
+            //set headers
+
+            const config = {
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`
+                }
+            }
+
+            const { data } = await axios.post(
+                `${api_URL}/goals/${userId}`, challengeData,config
+            )
+
+            return data;
+
+        }catch(err){
+            return rejectWithValue(err);
+        }
+
+    }
+)
 //view challenges(read)
 //update challenges
 //delete challenge
