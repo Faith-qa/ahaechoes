966408a566ab4899687351df47fc9b9bf7dbc109
Faith-qa <atienofaith12@gmail.com>
2024-09-08 11:09:14 +0300

    fixed galarry view

--- client/src/self/pages/More/journaling/JounLibrary/styles.ts
+++ client/src/self/pages/More/journaling/JounLibrary/styles.ts
@@ -2,18 +2,31 @@ import {StyleSheet} from "react-native";
 import {Dimensions} from "react-native";
 
 
-const { width } = Dimensions.get('window');
+export const { width } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
+    container: {
+        width: 390,
+        //paddingBottom:10,
+        flexDirection: "column",
+        alignItems: "flex-start",
+        //padding: 10, // Start items from the left side
+        //padding: 10,
+
+    },
     item: {
-        flex: 1,
-        margin: 2,
-        width: width / 3,
-        height: 150// Square grid items
+        //flex: 1,
+        //margin: 2,
+        width: width / 3 ,
+        height: 150,
+        alignSelf: 'flex-start', // Align items to the start of the row
+        //marginBottom: 20,
+
+// Square grid items
     },
     image: {
-        width: width/3 - 30,
-        height: 150,
+        width: "100%",
+        height: "100%",
     },
     audioCard: {
         justifyContent: 'center',
