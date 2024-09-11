b97b11577198d8d738e1050055654441fc2d4093
Faith-qa <atienofaith12@gmail.com>
2024-09-11 22:15:31 +0300

    fixed file upload avatar

--- server/habit-tracker/src/imageUpload-Cloudinary/cloudinary.provider.ts
+++ server/habit-tracker/src/imageUpload-Cloudinary/cloudinary.provider.ts
@@ -4,7 +4,7 @@ export const CloudinaryProvider = {
   provide: CLOUDINARY,
   useFactory: (): any => {
     return v2.config({
-      cloud_name: process.env.CLOUDi_NAME,
+      cloud_name: process.env.CLOUDI_NAME,
       api_key: process.env.CLOUDINARY_API_KEY,
       api_secret: process.env.CLOUDINARY_API_SECRET,
     });
